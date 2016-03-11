9e27a99a791d095673c5204a08bf29eb22502249
Cos <kkorenev@hotmail.com>
2016-03-11 20:21:59 +0000

    Adding the Get() for tweets

--- bot.js
+++ bot.js
@@ -6,3 +6,20 @@ var Twit = require('twit');
 var config = require('./config');
 
 var T = new Twit(config);
+
+
+//reading from twitter:
+var params = {
+  //key word + count
+  q: 'banana since:2011-07-11', count: 10
+}
+
+T.get('search/tweets', params, gotData);
+
+//printing to terminal
+function gotData(err, data, response) {
+  var tweets = data.statuses;
+  for (var i = 0; i < tweets.length; i++) {
+    console.log(tweets[i].text);
+  }
+}
