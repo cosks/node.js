c98e9638dc9a5b0a3f31ba02e1d1b83a2a8f0f9a
Cos <kkorenev@hotmail.com>
2016-03-15 21:02:47 +0000

    Saving changes

--- bot.js
+++ bot.js
@@ -4,41 +4,37 @@ var Twit = require('twit');
 
 // required fot authentication from Twit NPM
 var config = require('./config');
-
 var T = new Twit(config);
 
 
 //reading from twitter:
-var params = {
-  //key word + count
-  q: 'banana since:2011-07-11', count: 10
-}
-
-T.get('search/tweets', params, gotData);
-
-//printing to terminal
-function gotData(err, data, response) {
-  var tweets = data.statuses;
-  for (var i = 0; i < tweets.length; i++) {
-    console.log(tweets[i].text);
+  var params = {
+    //key word + count
+    q: '@TeamSoloMid since:2016-01-01', count: 4
   }
-}
+
+  T.get('search/tweets', params, gotData);
+
+  //printing to terminal
+  function gotData(err, data, response) {
+    var tweets = data.statuses;
+    for (var i = 0; i < tweets.length; i++) {
+      console.log(tweets[i].text);
+    }
+  }
 
 //  tweet 'hello world!'
-var tweet = {
-  status: '#helloWworld !!'
-}
+  var tweet = {
+    status: '#helloWworld !!'
+  }
 
-function tweeted(err, data, responce){
-  if(err){
-    console.log("Error"+err);
+  function tweeted(err, data, responce){
+    if(err){
+      console.log("Error"+err);
+    }
+    else{
+      //console.log(data);
+      console.log("Tweet posted!");
+    }
   }
-  else{
-    //console.log(data);
-    console.log("Tweet posted!");
-  }
-}
-T.post('statuses/update', tweet, tweeted)
-//T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
-//  console.log(data)
-//})
+  T.post('statuses/update', tweet, tweeted)
