b22350f311b04d25c7b87c28ff156bd9f93434c6
Cos <kkorenev@hotmail.com>
2016-03-30 17:03:20 +0100

    Saving changes

--- bot2.js
+++ bot2.js
@@ -11,6 +11,23 @@ tweetIt();
 setInterval(tweetIt(), 1000*60*60);
 
 function tweetIt(){
+
+  // upload the img to twitter first before making the tweet
+  function postImg(){
+    var filename = 'img/test.jpeg';
+    var params = {
+      encoding : 'base64'
+    }
+    var b64content = fs.readFileSync(filename, params);
+    //'uploaded' is the call back for when the information has been uploaded
+    T.post('media/upload', {media_data:b64content},uploaded);
+    function uploaded(err, data, responce){
+      // this is where the tweet will happen!!
+      
+    }
+  }
+
+
     var tweet = {
       status: '#helloWworld !!'
     }
