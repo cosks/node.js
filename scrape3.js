582580eabb1bea3306afb4bfa2159c9ebb331397
Cos <kkorenev@hotmail.com>
2016-04-07 23:45:12 +0100

    saving scrape3 update

--- scrape3.js
+++ scrape3.js
@@ -1,26 +1,20 @@
-var request = require('request'),
-    cheerio = require('cheerio'),
-    fs = require('fs');
+  var request = require('request'),
+      cheerio = require('cheerio'),
+      fs = require('fs');
 
-var images = [];
-console.log('ok0');
-request('https://www.reddit.com/r/gifs', accessImg());
-function accessImg(err, responce, body){
-  if (!err && responce.statusCode == 200) {
-    // save as a $ to make it seem more like jQuery due to cheerio
-    console.log("ok1");
-    var $ = cheerio.load(body);
-    //using an anonymus function
-    $('img','div.siteTable').each(saveImg());
+  var images = [];
+  request('https://www.reddit.com/r/gifs', function(err, responce, body){
+    if (!err && responce.statusCode == 200) {
+      // save as a $ to make it seem more like jQuery due to cheerio
+      var $ = cheerio.load(body);
+      //using an anonymus function
+      $('href','.thumbnail may-blank').each(function(){
+        var myImg = $(this).attr('src');
+        images.push(myImg);
+        //console.log(images);
+        for (var i = 0; i < images.length; i++) {
+          request(images[i]).pipe(fs.createWriteStream('img/swim'+ i + '.jpg'));
+        }
+      })
     }
-  }
-
-function saveImg(){
-  console.log("ok2");
-  var myImg = $(this).attr('src');
-  images.push(myImg);
-  // for (var i = 0; i < images.length; i++) {
-  //   request(images[i]).pipe(fs.createWriteStream('img/pic'+ i + '.gif'));
-  // }
-}
-  console.log(images);
+  });
