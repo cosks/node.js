7d41965e95ba09b5127c4cc647d7133a6a4677a5
Cos <kkorenev@hotmail.com>
2017-03-28 19:33:01 +0100

    saving learner app.js

---
+++ learner/app.js
@@ -0,0 +1,9 @@
+//testing for Node to work
+//an anonymous function:
+
+var printMsg = function(){
+	console.log("trial message!!");
+};
+
+//where we pass a function to a function
+setTimeout(printMsg, 5000);
