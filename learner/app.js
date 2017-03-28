d90e5afb6e43f38742792377c8de46a39080f84d
Cos <kkorenev@hotmail.com>
2017-03-28 19:49:20 +0100

    adding changes to app.js

--- learner/app.js
+++ learner/app.js
@@ -7,3 +7,25 @@ var printMsg = function(){
 
 //where we pass a function to a function
 setTimeout(printMsg, 5000);
+
+//*******************
+
+function placeOrder(orderNumber){
+	console.log("customer order: ",orderNumber)
+
+	deliver(function (){
+		console.log("Delivery made! Order: ",orderNumber);
+	});
+
+}
+//simulate a 3 seconds operation
+function deliver(callback){
+	setTimeout(callback,3000);
+}
+//simulate users web request
+placeOrder(1);
+placeOrder(2);
+placeOrder(3);
+placeOrder(4);
+placeOrder(5);
+placeOrder(6);
